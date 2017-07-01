79610c7fc80fa2d4feb93834d34e3fbe008b37e1
romeoenrico <enri.rome@yahoo.it>
2017-07-01 19:39:07 +0200

    improving footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -56,17 +56,25 @@
             <div class="row py-2">
               <div class="col-sm-3 hidden-md-down">
                 <a class="bg-circle bg-danger" href="# ">
-                  <i class="fa fa-2x fa-fw fa-file-pdf-o" aria-hidden="true "></i>
+                  <i class="fa fa-2x fa-fw fa fa-archive" aria-hidden="true "></i>
                 </a>
               </div>
               <div class="col-sm-9">
-                <h4>Download pdf</h4>
-                <p> You like print?</a></p>
+
+                <h4>Archives</h4>
+                <ul>
+                  @foreach ($archives as $stats)
+                  <li>
+                    <a href="#">{{ $stats['month'] . ' ' . $stats['year'] }}</a>
+                  </li>
+                  @endforeach
+                </ul>
+
 
               </div>
             </div>
             <!-- row ended -->
-            <!-- row starting  -->
+            <!-- row starting
             <div class="row py-2">
               <div class="col-sm-3 hidden-md-down">
                 <a class="bg-circle bg-info" href="https://twitter.com/ ">
@@ -78,7 +86,7 @@
                 About us.
               </div>
             </div>
-            <!-- row ended -->
+            row ended -->
           </div>
           <!-- footer column 3 end -->
         </div>
@@ -90,7 +98,12 @@
       <div class="container">
         <div class="row py-3">
           <div class="col-md-9">
-            <p class="text-white">Lorem ipsum.</p>
+
+            <p>
+              Lorem ipsum
+            </p>
+
+
           </div>
           <div class="col-md-3">
             <div class="d-inline-block">
